24f4acf742b69d03ff2ceb208314fddd47189df3
BecaudA <arthur.becaud@heig-vd.ch>
2020-06-23 15:52:43 +0200

    First musician package setup with UUID creation.y

--- docker/image-musician/musician-app/index.js
+++ docker/image-musician/musician-app/index.js
@@ -1,1 +1,14 @@
-console.log(`Test`);
+const dgram   = require('dgram');
+const socket  = dgram.createSocket('udp4');
+const net     = require('net');
+const { v4: uuidv4 } = require('uuid');
+
+const instruments = new Map();
+instruments.set("piano", "ti-ta-ti");
+instruments.set("trumpet", "pouet");
+instruments.set("flute", "trulu");
+instruments.set("violin", "gzi-gzi");
+instruments.set("drum", "boum-boum");
+
+const uuid = uuidv4();
+console.log(uuid)
