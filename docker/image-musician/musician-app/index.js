2d227ff155ee5b367117ab18580f3b9bdd2b9c9e
BecaudA <arthur.becaud@heig-vd.ch>
2020-06-23 16:09:56 +0200

    Added getSound function.
    Used to retrieve the correct instrument's sound from the
    instruments map.

--- docker/image-musician/musician-app/index.js
+++ docker/image-musician/musician-app/index.js
@@ -11,4 +11,23 @@ instruments.set("violin", "gzi-gzi");
 instruments.set("drum", "boum-boum");
 
 const uuid = uuidv4();
-console.log(uuid)
+console.log(uuid);
+
+console.log('Sound : ' + getInstrument());
+
+function getSound() {
+
+    if (process.argv.length != 3) {
+        console.log('Give this mad musician an instrument!');
+        process.exit(1);
+    }
+
+    sound = instruments.get(process.argv[2]);
+
+    if (sound == undefined) {
+        console.log('This musician doesn\'t know how to play : ' + process.argv[2]);
+        process.exit(1);
+    }
+
+    return sound;
+}
