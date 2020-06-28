666bd2a176f648e946fd9a9d805fcb6c4b56f80c
BecaudA <arthur.becaud@heig-vd.ch>
2020-06-28 23:31:10 +0200

    Cleaned the mess x).

--- docker/image-auditor/auditor-app/index.js
+++
@@ -1,4 +0,0 @@
-import { Auditor } from './auditor.js';
-import * as protocol from './protocol.js';
-
-let auditor = new Auditor(protocol.UDP_PORT, protocol.TCP_PORT, protocol.MUTLICAST_GROUP);
--- docker/image-musician/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker run res/musician violin
